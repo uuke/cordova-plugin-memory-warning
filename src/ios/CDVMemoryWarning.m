303c1f5a5c596454b236583e40e2f57c54396345
LiuPeng <tidycoder@gmail.com>
2016-03-16 10:56:54 +0800

    misc

--- src/ios/CDVMemoryWarning.m
+++ src/ios/CDVMemoryWarning.m
@@ -3,6 +3,13 @@
 
 @implementation CDVMemoryWarning 
 
+-(void) pluginInitialize{
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveMemoryWarning) name:@"UIApplicationDidReceiveMemoryWarning" object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onMemoryWarning) name:@"UIApplicationDidReceiveMemoryWarning" object:nil];
+
+}
+
  - (void)onMemoryWarning
 {
    // override to remove caches, etc
